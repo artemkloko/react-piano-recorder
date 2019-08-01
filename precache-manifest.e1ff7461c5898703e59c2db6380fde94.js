c11e5b9f3d0f381b91da227cb0b0435794b62ae9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-01 16:58:59 +0000

    Deploy artemkloko/react-piano-recorder to github.com/artemkloko/react-piano-recorder.git:gh-pages

---
+++ precache-manifest.e1ff7461c5898703e59c2db6380fde94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b808d311fd9d37f600dc604f28281e3f",
+    "url": "/react-piano-recorder/index.html"
+  },
+  {
+    "revision": "3abb26646efdeef99598",
+    "url": "/react-piano-recorder/static/css/2.569cb674.chunk.css"
+  },
+  {
+    "revision": "a5c8065a24f86d1441e7",
+    "url": "/react-piano-recorder/static/css/main.a09defc4.chunk.css"
+  },
+  {
+    "revision": "3abb26646efdeef99598",
+    "url": "/react-piano-recorder/static/js/2.d63408c7.chunk.js"
+  },
+  {
+    "revision": "a5c8065a24f86d1441e7",
+    "url": "/react-piano-recorder/static/js/main.f2494b70.chunk.js"
+  },
+  {
+    "revision": "67d0b2eab6caa1bd8889",
+    "url": "/react-piano-recorder/static/js/runtime~main.c5260f29.js"
+  }
+]);
